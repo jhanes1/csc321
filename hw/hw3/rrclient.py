0cb4f5aea007c4cc5232e8478f0d1dc34a7aea16
jhanes1 <jdhanes1@gmail.com>
2016-10-27 09:07:57 -0500

    Initial Commit of Lab

---
+++ hw/hw3/rrclient.py
@@ -0,0 +1,19 @@
+#
+#   Request-reply client in Python
+#   Connects REQ socket to tcp://localhost:5559
+#   Sends "Hello" to server, expects "World" back
+#
+import zmq
+
+#  Prepare our context and sockets
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5559")
+
+#  Do 10 requests, waiting each time for a response
+request = 0
+while True:
+    socket.send(b"Hello")
+    message = socket.recv()
+    print("Received reply %s [%s]" % (request, message))
+    request += 1

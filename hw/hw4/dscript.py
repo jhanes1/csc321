ec928cd78a6ddb109f8ee8b439fee438ee244c36
jhanes1 <jdhanes1@gmail.com>
2016-11-15 11:30:38 -0600

    final v1 of domain script, edges, ip edges, and ptr edges.

--- hw/hw4/dscript.py
+++ hw/hw4/dscript.py
@@ -1,6 +1,6 @@
-import os
-import subprocess
+import subprocess, collections
 
+## RIP/EDIT from old assignment
 from functools import lru_cache
 import csv
 memoize = lru_cache(1)
@@ -12,27 +12,104 @@ def read_domains():
         domains = list(reader)
     return domains
 
-domainsDict = read_domains()
-del domainsDict[0]
-print(domainsDict[0])
+## Runs host commands and returns their output
+def checkHost(ID):
+    result = subprocess.check_output("host " + ID, shell = True)
+    result = str(result,'utf-8').split('\n')
+    return result
 
-#holder = os.system("host "+domainsDict[0]['domain'])
-dHolder = domainsDict[0]['domain']
-holder = subprocess.check_output("host "+dHolder,shell=True)
-holder = str(holder,'utf-8').split('\n')
-containing = [line for line in holder if "has address" in line]
-print(containing)
-for item in containing:
-    tItem = item.split(' ')
-    ip = tItem[len(tItem)-1]
-    print(ip)
+domainsDict = read_domains()
+del domainsDict[0] ## CLEANUP for header row in file
+
+## COLLECTORS for file outputs
+tString = ""
+idDicts = collections.OrderedDict() #allows sorting dictionary
+ridDicts = collections.OrderedDict() #allows sorting dictionary
+
+## ITER for domains
+dDict = {}
+for i in range(len(domainsDict)):
+    currD = domainsDict[i]['domain']
+    print(currD) ## NOTIFY FOR DOING THINGS
+    dDict[currD] = []
+
+    ## CATCH no IPs for domain
     try:
-        test = subprocess.check_output("host "+ip,shell=True)
+        holder = checkHost(currD)
     except:
-        test = b""
-    ipContainer = str(test,'utf-8').split('\n')
-    print(ipContainer)
-    for item2 in ipContainer:
-        tItem2 = item2.split(' ')
-        reverseIP = tItem2[len(tItem2)-1]
-        print(reverseIP)
+        holder = ""
+
+    ## ITER for all IPs
+    ipDict = {}
+    for IPs in [line for line in holder if "has address" in line]:
+        ## Isolate ip
+        tIPs = IPs.split(' ')
+        ip = tIPs[len(tIPs)-1]
+        ## CREATE dDicts
+        ipDict[ip] = []
+
+        ## FILEIN ipEdges.tsv
+        if ip in idDicts.keys():
+            idDicts[ip].append(currD)
+        else:
+            idDicts[ip] = [currD]
+
+        ## CATCH no pointers for ip
+        try:
+            ipContainer = checkHost(ip)
+        except:
+            ipContainer = [""]
+
+        ## CLEANUP
+        if ipContainer[len(ipContainer)-1] == '':
+            del ipContainer[len(ipContainer)-1]
+
+        ## ITER for all ptrs
+        for ptr in ipContainer:
+            ## Isolate ptr
+            tptr = ptr.split(' ')
+            reverseIP = tptr[len(tptr)-1]
+
+            if reverseIP == " ": ## CLEANUP bad returns
+                pass
+            elif reverseIP.count('.') < 2: ## CLEANUP bad returns
+                pass
+            else:
+                ## CREATE dDicts
+                ipDict[ip].append(reverseIP)
+
+                ## FILEIN edges.tsv
+                tString += (currD + '\t' + ip + '\t' + reverseIP + '\n')
+
+                ## FILEIN ripEdges.tsv
+                if reverseIP in ridDicts.keys():
+                    ridDicts[reverseIP].append(currD)
+                else:
+                    ridDicts[reverseIP] = [currD]
+    ## CREATE dDicts
+    dDict[currD].append(ipDict)
+
+## INFO dDicts contains dictionary of all results
+## INFO idDicts contains domains that use specific IP's
+## INFO ridDicts contains domains that use specific reverseIP's
+
+## WRITE BLOCK
+with open('edges.tsv','wt') as wtp:
+    wtp.write(tString)
+    wtp.close()
+
+with open('ipEdges.tsv','wt') as wtp:
+    tString = ""
+    tDict = collections.OrderedDict(sorted(idDicts.items(),key=lambda t: t[0]))
+    for key, value in tDict.items():
+        tString += key + "\t" + str(value) + '\n'
+    wtp.write(tString)
+    wtp.close()
+
+with open('ripEdges.tsv','wt') as wtp:
+    tString = ""
+    tDict = collections.OrderedDict(sorted(ridDicts.items(),key=lambda t: t[0]))
+    for key, value in tDict.items():
+        tString += key + "\t" + str(value) + '\n'
+    wtp.write(tString)
+    wtp.close()
